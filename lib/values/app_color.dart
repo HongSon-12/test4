ed6aec896634fca18863cc3518901c14140ea82b
minhnhattt <n.minhnhat2304@gmail.com>
2022-03-08 02:57:45 +0700

    hehe

--- lib/values/app_color.dart
+++ lib/values/app_color.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class AppColors {
+  static const primaryColor = Color(0xffBADAF8);
+  static const greyColor = Color(0xff8A8A8A);
+  static const btnColor = Color(0xff00A1FC);
+  static const randomColor1 = Color(0xffBADAF8);
+  static const randomColor2 = Color(0xffF3D1DC);
+  static const randomColor3 = Color(0xff8FD9D1);
+  static const randomColor4 = Color(0xffF1E0B0);
+  static const randomColor5 = Color(0xffADA2F2);
+  static const randomColor6 = Color(0xffE6DCE5);
+  static const randomColor7 = Color(0xff38908F);
+  static const randomColor8 = Color(0xffFFBFA3);
+}
